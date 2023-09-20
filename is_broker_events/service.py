c3a4b623acbb0fa57179714f22d18328cfa5ec24
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-09-20 14:22:23 -0300

    refactor: reorganize project and update libraries

---
+++ is_broker_events/service.py
@@ -0,0 +1,99 @@
+import sys
+from urllib.parse import urljoin
+
+import requests
+from google.protobuf.json_format import Parse, ParseError
+from is_msgs.common_pb2 import ConsumerList
+from is_wire.core import Channel, Message, Subscription
+
+from is_broker_events.conf.options_pb2 import BrokerEventsOptions
+from is_broker_events.logger import Logger
+
+
+class BrokerEvents(object):
+
+    def __init__(self, options: BrokerEventsOptions) -> None:
+        self.log = Logger(name="BrokerEvents")
+        self.log.debug("Connecting to RabbitMQ broker at: '{}'", options.broker_uri)
+
+        self.channel = Channel(uri=options.broker_uri, exchange="is")
+        self.subscription = Subscription(channel=self.channel, name="BrokerEvents")
+        self.subscription.subscribe(topic="binding.*")
+
+        self.consumers = self.query_consumers_http(management_uri=options.management_uri)
+        self.log.debug("Got list of consumers at: '{}'", options.management_uri)
+
+    def run(self) -> None:
+        while True:
+            msg = self.channel.consume()
+            self.log.debug("topic='{}' metadata={}", msg.topic, msg.metadata)
+
+            if msg.metadata["destination_kind"] != "queue" or \
+               msg.metadata["source_name"] != "is":
+                continue
+
+            event = msg.topic.split('.')[-1]
+            topic = msg.metadata["routing_key"]
+            queue = msg.metadata["destination_name"]
+
+            if event == "created":
+                self.consumers.info[topic].consumers.append(queue)
+            elif event == "deleted":
+                self.consumers.info[topic].consumers.remove(queue)
+                if len(self.consumers.info[topic].consumers) == 0:
+                    del self.consumers.info[topic]
+
+            self.log.info("event='{}' topic='{}' queue='{}'", event, topic, queue)
+
+            self.channel.publish(
+                Message(content=self.consumers),
+                topic="BrokerEvents.Consumers",
+            )
+
+    @staticmethod
+    def query_consumers_http(management_uri: str) -> ConsumerList:
+        url = urljoin(management_uri, "/api/bindings")
+        reply = requests.get(url=url, timeout=5)
+        # raise expection if failed to get list of bindings
+        # in management API at RabbitMQ Broker
+        reply.raise_for_status()
+        bindings = reply.json()
+        bindings = [
+            b for b in bindings if b["destination_type"] == "queue" and b["source"] == "is"
+        ]
+        consumers = ConsumerList()
+        for binding in bindings:
+            consumers.info[binding["routing_key"]].consumers.append(binding["destination"])
+        return consumers
+
+
+def load_json(logger: Logger,  # type: ignore[return]
+              path: str = "/etc/is-broker-events/options.json") -> BrokerEventsOptions:
+    try:
+        with open(file=path, mode='r', encoding="utf-8") as file:
+            try:
+                options = Parse(file.read(), BrokerEventsOptions())
+                logger.info('BrokerEventsOptions: \n{}', options)
+                return options
+            except ParseError as ex:
+                logger.critical('Unable to load options from \'{}\'. \n{}', path, ex)
+    except FileNotFoundError:
+        logger.critical('Unable to open file \'{}\'', path)
+
+
+def main() -> None:
+    if len(sys.argv) > 1:
+        options_filename = sys.argv[1]
+    else:
+        options_filename = '/etc/is-broker-events/options.json'
+    logger = Logger(name="BrokerEvents")
+    options = load_json(
+        logger=logger,
+        path=options_filename,
+    )
+    service = BrokerEvents(options=options)
+    service.run()
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='is_broker_events',
-    version='0.0.3',
+    version='0.0.4',
     description='',
-    url='http://github.com/labviros/is-broker-events',
-    author='labviros',
+    url='http://github.com/labvisio/is-broker-events',
+    author='labvisio',
     license='MIT',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=[
+        'is_broker_events',
+        'is_broker_events.conf',
+    ],
+    package_dir={'': '.'},
     entry_points={
         'console_scripts': [
             'is-broker-events=is_broker_events.service:main',
@@ -16,8 +19,14 @@ setup(
     },
     zip_safe=False,
     install_requires=[
-        'is-wire==1.1.2',
-        'is-msgs==1.1.5',
-        'requests==2.20.0',
+        'is-wire==1.2.1',
+        'is-msgs==1.1.18',
+        'requests==2.31.0',
+        'six==1.16.0',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3 :: Only',
     ],
 )
